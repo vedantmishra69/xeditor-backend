ed766f7ff096a0dac5a370063f155b0cafd9a433
Vedant Mishra <vedantmishra050302@gmail.com>
2024-11-14 02:10:08 +0530

    added doc persistence

--- routes/socket.route.js
+++ routes/socket.route.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const hocusServer = require("../lib/hocus");
+const { rooms } = require("../lib/chat");
 
-router.ws("/", (websocket, request) => {
-  const context = {
-    user: {
-      id: 1234,
-      name: "Jane",
-    },
-  };
-
-  hocusServer.handleConnection(websocket, request, context);
+router.ws("/doc", (websocket, request) => {
+  hocusServer.handleConnection(websocket, request);
 });
+
+// router.ws("/chat", (ws, req) => {
+//   const roomId = req.headers["sec-websocket-protocol"];
+//   if (!rooms.has(roomId)) rooms.set(roomId, []);
+//   rooms.get(roomId).push(ws);
+// });
 
 module.exports = router;
