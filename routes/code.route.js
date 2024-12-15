19ee31b365fd5dd5fe35b522bfb30fa528164ef8
Vedant Mishra <vedantmishra050302@gmail.com>
2024-12-16 00:18:36 +0530

    added auth

--- routes/code.route.js
+++ routes/code.route.js
@@ -4,9 +4,11 @@ const {
   getSubmission,
   createSubmission,
 } = require("../controllers/code.controller");
+const { protect } = require("../lib/middleware");
 
 router.get("/langs", getLangs);
 router.get("/result", getSubmission);
+router.use(protect);
 router.post("/submit", createSubmission);
 
 module.exports = router;
