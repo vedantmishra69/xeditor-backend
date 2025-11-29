a4bf757b84c7f730f3d7cfee72c578b2cb8c4858
Vedant Mishra <vedantmishra050302@gmail.com>
2025-11-29 19:44:34 +0530

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker stop xeditor-backend
 docker rm xeditor-backend
 docker image rm xeditor-backend
 docker build -t xeditor-backend .
-docker run --name xeditor-backend -d -p 8000:8000 xeditor-backend
+docker run --name xeditor-backend -d --restart=always -p 8000:8000 xeditor-backend
 
